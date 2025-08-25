f72165a721ef5c3917a8a27229c2d7fa12eb8093
Boshen <boshenc@gmail.com>
2025-08-25 18:03:03 +0800

    fix

--- benches/vpath_bench.rs
+++ benches/vpath_bench.rs
@@ -1,5 +1,6 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use pnp::fs::VPath;
+use std::hint::black_box;
 use std::path::Path;
 use std::time::Duration;
 
