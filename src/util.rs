8ff03a9262a5344deeb7e2494311e3a8ddc90292
JounQin <admin@1stg.me>
2025-09-10 17:43:35 +0800

    test: add a new test case for global cache (#10)
    * chore: reproduce global cache issue
    
    * Fixes relative drive paths
    
    * Removes debug
    
    * Fixes tidbits
    
    * Removes debug
    
    * Fixes mistakes
    
    * Attempts to support UNC paths
    
    * Merge conflict
    
    ---------
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- src/util.rs
+++ src/util.rs
@@ -2,8 +2,9 @@ use fancy_regex::Regex;
 use serde::{Deserialize, Deserializer, de::Error};
 use std::borrow::Cow;
 
-use path_slash::PathBufExt;
-use std::path::{Path, PathBuf};
+use std::path::{MAIN_SEPARATOR_STR, Path, PathBuf};
+#[cfg(windows)]
+use std::sync::LazyLock;
 
 #[derive(Debug, Default, Clone)]
 pub struct Trie<T> {
@@ -33,17 +34,112 @@ impl<T> Trie<T> {
     }
 }
 
+#[cfg(windows)]
+static WINDOWS_PATH_REGEXP: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^([a-zA-Z]:.*)$").unwrap());
+#[cfg(windows)]
+static UNC_WINDOWS_PATH_REGEXP: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^[\/\\][\/\\](\.[\/\\])?(.*)$").unwrap());
+#[cfg(windows)]
+static PORTABLE_PATH_REGEXP: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^\/([a-zA-Z]:.*)$").unwrap());
+#[cfg(windows)]
+static UNC_PORTABLE_PATH_REGEXP: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^\/unc\/(\.dot\/)?(.*)$").unwrap());
+
+fn from_portable_path<'a>(str: &'a str) -> Cow<'a, str> {
+    #[cfg(windows)]
+    {
+        if let Ok(Some(caps)) = PORTABLE_PATH_REGEXP.captures(str) {
+            return Cow::Borrowed(caps.get(1).unwrap().as_str());
+        }
+
+        if let Ok(Some(caps)) = UNC_PORTABLE_PATH_REGEXP.captures(str) {
+            if caps.get(1).is_some() {
+                return Cow::Owned(format!("\\\\.\\{}", caps.get(2).unwrap().as_str()));
+            } else {
+                return Cow::Owned(format!("\\\\{}", caps.get(2).unwrap().as_str()));
+            }
+        }
+    }
+
+    Cow::Borrowed(str)
+}
+
+fn to_portable_path<'a>(str: &'a str) -> Cow<'a, str> {
+    #[cfg(windows)]
+    {
+        if let Ok(Some(caps)) = WINDOWS_PATH_REGEXP.captures(str) {
+            return Cow::Owned(format!("/{}", caps.get(1).unwrap().as_str()));
+        }
+
+        if let Ok(Some(caps)) = UNC_WINDOWS_PATH_REGEXP.captures(str) {
+            if caps.get(1).is_some() {
+                return Cow::Owned(format!("/unc/.dot/{}", caps.get(2).unwrap().as_str()));
+            } else {
+                return Cow::Owned(format!("/unc/{}", caps.get(2).unwrap().as_str()));
+            }
+        }
+    }
+
+    Cow::Borrowed(str)
+}
+
 pub fn normalize_path<P: AsRef<str>>(original: P) -> String {
-    let original_str = original.as_ref();
+    let original_str = to_portable_path(original.as_ref());
 
-    let p = PathBuf::from(original_str);
-    let mut str = clean_path::clean(p).to_slash_lossy().to_string();
+    let check_str_root = original_str.strip_prefix("/");
+    let str_minus_root = check_str_root.unwrap_or(original_str.as_ref());
 
-    if original_str.ends_with('/') && !str.ends_with('/') {
+    let components = str_minus_root.split(&['/', '\\'][..]);
+
+    let mut out: Vec<&str> = Vec::new();
+
+    for comp in components {
+        match comp {
+            "" | "." => {
+                // Those components don't progress the path
+            }
+
+            ".." => match out.last() {
+                None if check_str_root.is_some() => {
+                    // No need to add a ".." since we're already at the root
+                }
+
+                Some(&"..") | None => {
+                    out.push(comp);
+                }
+
+                Some(_) => {
+                    out.pop();
+                }
+            },
+
+            comp => out.push(comp),
+        }
+    }
+
+    if check_str_root.is_some() {
+        if out.is_empty() {
+            return "/".to_string();
+        } else {
+            out.insert(0, "");
+        }
+    }
+
+    let mut str = out.join("/");
+
+    if out.is_empty() {
+        return ".".to_string();
+    }
+
+    if (original_str.ends_with('/') || original_str.ends_with(MAIN_SEPARATOR_STR))
+        && !str.ends_with('/')
+    {
         str.push('/');
     }
 
-    str
+    from_portable_path(&str).into_owned()
 }
 
 #[cfg(test)]
@@ -59,13 +155,31 @@ mod tests {
         assert_eq!(normalize_path("foo//bar"), "foo/bar");
         assert_eq!(normalize_path("foo/./bar"), "foo/bar");
         assert_eq!(normalize_path("foo/../bar"), "bar");
+        assert_eq!(normalize_path("foo/..//bar"), "bar");
         assert_eq!(normalize_path("foo/bar/.."), "foo");
         assert_eq!(normalize_path("foo/../../bar"), "../bar");
         assert_eq!(normalize_path("../foo/../../bar"), "../../bar");
+        assert_eq!(normalize_path("foo/../../bar"), "../bar");
         assert_eq!(normalize_path("./foo"), "foo");
         assert_eq!(normalize_path("../foo"), "../foo");
+        assert_eq!(normalize_path("../D:/foo"), "../D:/foo");
         assert_eq!(normalize_path("/foo/bar"), "/foo/bar");
+        assert_eq!(normalize_path("/foo/../../bar/baz"), "/bar/baz");
+        assert_eq!(normalize_path("/../foo/bar"), "/foo/bar");
+        assert_eq!(normalize_path("/../foo/bar//"), "/foo/bar/");
         assert_eq!(normalize_path("/foo/bar/"), "/foo/bar/");
+
+        #[cfg(windows)]
+        assert_eq!(normalize_path("D:\\foo\\..\\bar"), "D:/bar");
+        #[cfg(windows)]
+        assert_eq!(normalize_path("D:\\foo\\..\\..\\C:\\bar\\test"), "C:/bar/test");
+        #[cfg(windows)]
+        assert_eq!(normalize_path("\\\\server-name\\foo\\..\\bar"), "\\\\server-name/bar");
+        #[cfg(windows)]
+        assert_eq!(
+            normalize_path("\\\\server-name\\foo\\..\\..\\..\\C:\\bar\\test"),
+            "C:/bar/test"
+        );
     }
 }
 
